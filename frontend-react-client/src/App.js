6b27021f5f5e35e0699eafe3cab8e87efbe5f106
nmarkoborodova <ewewraw@gmail.com>
2023-12-02 16:15:16 +0100

    added add=digest page

--- frontend-react-client/src/App.js
+++ frontend-react-client/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Link, Routes, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home';
 import Page2 from './pages/page2';
 import Navbar from './components/navbar';
+import AddDigestPage from "./pages/add-digest";
 
 function App() {
 
-  return (
-    <div className="bg-background min-h-screen">
-      <BrowserRouter className="flex h-full w-full"> 
-        <Navbar/>
-        <Routes>
-          <Route path="/" Component={HomePage}/>
-          <Route path="/page2" Component={Page2}/>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="bg-background min-h-screen">
+            <BrowserRouter className="flex h-full w-full">
+                <Navbar/>
+                <Routes>
+                    <Route path="/" Component={HomePage}/>
+                    <Route path="/page2" Component={Page2}/>
+                    <Route path="/add-digest" Component={AddDigestPage}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
