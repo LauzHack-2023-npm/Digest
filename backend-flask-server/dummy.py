927d997f9f59dd56bba05ae431d3f4231b9241c7
philippwulff <philipp.sylt@gmail.com>
2023-12-03 09:07:10 +0100

    minor

--- backend-flask-server/dummy.py
+++ backend-flask-server/dummy.py
@@ -1,4 +1,9 @@
 from utils import create_digest_dict, create_episode_dict
+import json
+
+
+with open("sources.json", "r") as sources_file:
+    all_sources = json.load(sources_file)
 
 
 DUMMY_DIGEST_SEQUENCE = [
@@ -10,19 +15,28 @@ DUMMY_DIGEST_SEQUENCE = [
         "formal",
         "customNarrationStyle",
         "2021-01-01",
-        [], 
+        sources=[_["name"] for _ in all_sources], 
         episodes=[
             create_episode_dict(
                 "Episode 1",
                 "Episode 1 description",
                 "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
-                "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+                "https://mir-s3-cdn-cf.behance.net/project_modules/1400/fe529a64193929.5aca8500ba9ab.jpg",
                 "00:00:00",
                 "2021-01-01",
                 [],
                 False,
             ),
-            # TODO
+            create_episode_dict(
+                "Episode 1",
+                "Episode 1 description",
+                "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+                "https://mir-s3-cdn-cf.behance.net/project_modules/1400/fe529a64193929.5aca8500ba9ab.jpg",
+                "00:00:00",
+                "2021-01-01",
+                [],
+                False,
+            ),
         ],
     ),
 ]
