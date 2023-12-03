0a25d489ce7791bd3be7548b4adfddf63936a3ad
philippwulff <philipp.sylt@gmail.com>
2023-12-03 06:46:20 +0100

    cleanup and todos

--- backend-flask-server/app.py
+++ backend-flask-server/app.py
@@ -6,10 +6,65 @@ from utils import generate_digest_data, generateKeyword, getSourceName
 from dummy import DUMMY_DIGEST_SEQUENCE, create_digest_dict, create_episode_dict
 import datetime
 
+
 app = Flask(__name__)
 
-@app.route('/api/generate-digest-podcast', methods=['POST'])
-def generate_digest_podcast():
+
+@app.route('/api/get-dummy-digest-sequences')
+def get_dummy_digest_sequences():
+    """Returns some dummy content to populate the user's choices with."""
+    try:
+        return jsonify(DUMMY_DIGEST_SEQUENCE), 200
+    except Exception as e:
+        print(f"[get_dummy_digest_sequences] Exception: {e}")
+        return {"error": str(e)}, 500
+    
+    
+@app.route('/api/get-available-sources')
+# get prompt of the user
+# returns possible sources
+def get_available_sources():
+    return jsonify([
+        {'name': 'wikipedia', 'url': 'https://en.wikipedia.org/'},
+        {'name': 'arxive', 'url': 'https://arxiv.org/'}
+    ])
+    
+
+@app.route('/api/get-digest-with-sources', methods=['POST'])
+def post_digest_sequence():
+    try:
+        data = request.get_json()
+
+        # Extract data from the JSON payload
+        digestName = data.get("digestName")
+        digestDescription = data.get("digestDescription")
+
+        with open("sources.json", "r") as sources_file:
+            sources = json.load(sources_file)
+            sourceName = getSourceName(digestName, digestDescription)
+            sources = list(filter(lambda x: x["name"] == sourceName, sources))
+        
+        ret = create_digest_dict(
+            digestName,
+            digestDescription,
+            data.get("contentFrequency"),
+            data.get("customFrequency"),
+            data.get("contentNarrationStyle"),
+            data.get("customNarrationStyle"),
+            data.get("createdAt"),
+            [source["name"] for source in sources], # list of string of names
+            data.get("episodes"),
+        )
+
+        return jsonify(ret), 200
+
+    except Exception as e:
+        print('Error processing request:', str(e))
+        return jsonify({'error': 'Internal Server Error'}), 500
+    
+    
+@app.route('/api/generate-digest-content', methods=['POST'])
+def generate_digest_content():
     try:
         data = request.get_json()
 
@@ -72,79 +127,26 @@ def generate_digest_podcast():
     except Exception as e:
         print('Error processing request:', str(e))
         return jsonify({'error': 'Internal Server Error'}), 500
-    
-
-@app.route('/api/get-dummy-digest-sequences')
-def get_dummy_digest_sequences():
-    """Returns some dummy content to populate the user's choices with."""
-    try:
-        return jsonify(DUMMY_DIGEST_SEQUENCE), 200
-    except Exception as e:
-        print(f"[get_dummy_digest_sequences] Exception: {e}")
-        return {"error": str(e)}, 500
 
 
-@app.route('/api/get-digest-with-sources', methods=['POST'])
-def post_digest_sequence():
-    try:
-        data = request.get_json()
+# @app.route('/api/generate-digest-content', methods = ['POST'])
+# def post_sources():
+#     data = request.get_json()
+#     digestName = data.get("digestName")
+#     digestDescription = data.get("digestDescription")
+#     contentFrequency = data.get("customFrequency") if data.get("customFrequency") is not None and data.get("customFrequency") != "" else data.get("contentFrequency")
+#     narrationStyle = data.get("customNarrationStyle") if data.get("customNarrationStyle") is not None and data.get("customNarrationStyle") != "" else data.get("narrationStyle")
+#     sources = data.get("sources")
+#     customSources = data.get("customSources")
 
-        # Extract data from the JSON payload
-        digestName = data.get("digestName")
-        digestDescription = data.get("digestDescription")
-
-        with open("sources.json", "r") as sources_file:
-            sources = json.load(sources_file)
-            sourceName = getSourceName(digestName, digestDescription)
-            sources = list(filter(lambda x: x["name"] == sourceName, sources))
-        
-        ret = create_digest_dict(
-            digestName,
-            digestDescription,
-            data.get("contentFrequency"),
-            data.get("customFrequency"),
-            data.get("contentNarrationStyle"),
-            data.get("customNarrationStyle"),
-            data.get("createdAt"),
-            [source["name"] for source in sources], # list of string of names
-            data.get("episodes"),
-        )
-
-        return jsonify(ret), 200
-
-    except Exception as e:
-        print('Error processing request:', str(e))
-        return jsonify({'error': 'Internal Server Error'}), 500
-
-
-@app.route('/api/generate-digest-content', methods = ['POST'])
-def post_sources():
-    data = request.get_json()
-    digestName = data.get("digestName")
-    digestDescription = data.get("digestDescription")
-    contentFrequency = data.get("customFrequency") if data.get("customFrequency") is not None and data.get("customFrequency") != "" else data.get("contentFrequency")
-    narrationStyle = data.get("customNarrationStyle") if data.get("customNarrationStyle") is not None and data.get("customNarrationStyle") != "" else data.get("narrationStyle")
-    sources = data.get("sources")
-    customSources = data.get("customSources")
-
-    return {
-    'digestName': digestName,
-    'digestDescription': digestDescription,
-    'contentFrequency': contentFrequency,
-    'narrationStyle': narrationStyle,
-    'sources': sources,
-    'customSources': customSources
-    }
-
-
-@app.route('/api/get-available-sources')
-# get prompt of the user
-# returns possible sources
-def get_available_sources():
-    return jsonify([
-        {'name': 'wikipedia', 'url': 'https://en.wikipedia.org/'},
-        {'name': 'arxive', 'url': 'https://arxiv.org/'}
-    ])
+#     return {
+#     'digestName': digestName,
+#     'digestDescription': digestDescription,
+#     'contentFrequency': contentFrequency,
+#     'narrationStyle': narrationStyle,
+#     'sources': sources,
+#     'customSources': customSources
+#     }
 
 
 if __name__ == '__main__':
